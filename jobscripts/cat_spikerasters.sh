e8ca58c9ed6e337e7ad72bbc48e31c5c3cc583b0
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2021-11-05 17:37:42 +0100

    Adding randomness to Current-shot stimulation protocol

--- jobscripts/cat_spikerasters.sh
+++ jobscripts/cat_spikerasters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file can not be launched with sh,
-# since it contatins directory changes.
+# since it contains directory changes.
 #
 # Instead, it should be run by using:
 # source cat_spikerasters.sh
@@ -8,12 +8,13 @@
 # or simply by using the source alias (.):
 # . cat_spikerasters.sh
 
-for j in $(seq 15 15)
+Duration=3
+for j in $(seq 11 11)
   do
-    for i in $(seq 0 6)
+    for i in $(seq 0 0)
       do
-        PercentArtCells=${j}_${i}
-        result_dir="run_Sasaki_Scale_100_CurrentShotStim_0${PercentArtCells}_SimDuration_20_Procs_8"
+        PercentArtCells=${j}.${i}
+        result_dir="run_Sasaki_Scale_100_CurrentShotStim_0${j}_${i}_SimDuration_${Duration}_Procs_8"
         cd ./results/${result_dir}
         echo ${PWD}
         cat spikeraster_{0..7}.dat | sort > spikeraster.dat
--- jobscripts/run_CurrentShotStims.sh
+++ jobscripts/run_CurrentShotStims.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+# This file can not be launched with sh,
+# since it contains random number generations.
+#
+# Instead, it should be run by using:
+# source run_CurrentShotStims.sh
+#
+# or simply by using the source alias (.):
+# . run_CurrentShotStims.sh
 
-for j in $(seq 10 20)
+Duration=20
+RANDOM=$(date +%s)
+for j in $(seq 12 12)
   do
-    for i in $(seq 0 9)
+    for i in $(seq 7 9)
       do
         PercentArtCells=${j}.${i}
-        Duration=20
         result_dir="run_Sasaki_Scale_100_CurrentShotStim_0${j}_${i}_SimDuration_${Duration}_Procs_8"
         mpiexec -n 8 x86_64/special -nobanner -nogui -mpi -c "strdef RunName" -c RunName="\"${result_dir}\"" \
                                                           -c "Scale=100" -c "SimDuration=${Duration}" \
-                                                          -c "PercentArtActive=${PercentArtCells}" -c "JobHours=25" main.hoc
+                                                          -c "PercentArtActive=${PercentArtCells}" \
+                                                          -c "RandomSeedsCell=${RANDOM}" -c "JobHours=25" main.hoc
+        # Concatenate all spikeraster data
+        cd ./results/${result_dir}
+        echo ${PWD}
+        cat spikeraster_{0..7}.dat | sort > spikeraster.dat
+        # rm spikeraster.dat
+        cd ../..
+
       done
   done
